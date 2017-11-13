0b3410ab68ccc704ca3b7b03ff09291f38648d72
Jbok <0614bok@gmail.com>
2017-11-14 02:16:31 +0900

    Doing IPC Function... May be I think It will be better doing another things...

--- src/commands.c
+++ src/commands.c
@@ -8,8 +8,21 @@
 #include <sys/wait.h>	//wait
 #include <signal.h>
 
+//IPC
+#include <sys/socket.h> //socket
+#include <pthread.h> //thread
+
 #include "commands.h"
 #include "built_in.h"
+
+//thread function
+void* thr_fn(char argv[]){
+	char *com[2];
+	strcpy(com[0],argv);
+	com[1]=NULL;
+	execv(com[0],com);
+	return NULL;	
+}
 
 
 
@@ -33,13 +46,15 @@ static int is_built_in_command(const char* command_name)
   return -1; // Not found
 }
 
+
+
 /*
  * Description: Currently this function only handles single built_in commands. You should modify this structure to launch process and offer pipeline functionality.
  */
 int evaluate_command(int n_commands, struct single_command (*commands)[512])
 {
 
-  
+
 
   if (n_commands > 0) {	
 
@@ -56,13 +71,32 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
       } else {
         fprintf(stderr, "%s: Invalid arguments\n", com->argv[0]);
         return -1;
-      }
+  }
     } else if (strcmp(com->argv[0], "") == 0) {
       return 0;
     } else if (strcmp(com->argv[0], "exit") == 0) {
       return 1;
     } else {
-      
+ 	/////////////////////////////////////////////   
+	//Inter-Process Communication and Threading//
+	/////////////////////////////////////////////
+	if( n_commands > 1){
+		
+		pthread_t thread;
+		char buf[8096];
+		strcpy(buf,(com+1)->argv[0]);
+		printf("%s\n",buf);
+		if(pthread_create(&thread, NULL, thr_fn,buf)!=0){//tread error
+			fprintf(stderr,"Error about thread occurs\n");
+		}else{
+		}
+		int pipefd[2];
+		int pid;
+		
+		
+
+	}
+	
 // NewLine for Process Creation
 	int Is_bg=-1; //Is background processing? Yes=1, No=-1 
 	if(!strcmp(com->argv[com->argc-1],"&")){
@@ -70,6 +104,8 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 		(com->argc)--; //delete '&'
 		Is_bg=1;
 	}
+
+
 	//Path Resolution
 	char path[5][512]={
 		"/usr/local/bin/",
@@ -84,9 +120,8 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 
       if(pid == -1){
 	fprintf(stderr,"Error about fork occurs\n");
-        exit(1); 
+        _exit(EXIT_FAILURE); 
       } else if (pid == 0){  //child
-	printf("%d\n",getpid());
 
 	//Path Resolution
 
@@ -101,14 +136,16 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 	}
 
         fprintf(stderr, "%s: command not found\n", com->argv[0]);
-	exit(0);
+	_exit(EXIT_FAILURE);
 
       } else{	//parent
 	//Background Processing
 	if(Is_bg==1){
-	  
+		
 	} else{ //Not Background
-	  waitpid(pid,0,0); 
+	  waitpid(pid,NULL,0);
+	  tcsetpgrp(STDIN_FILENO, getpid());
+	  fflush(stdout); 
 	}
       }
     }
--- src/main.c
+++ src/main.c
@@ -14,15 +14,19 @@
 #include "utils.h"
 #include "signal_handlers.h"
 
+
 void bg_handler(int signo)
 {	
 	pid_t pid;
 	int stat;
 	
-	while( (pid= waitpid(-1, &stat ,WNOHANG))> 0 )
+	while( (pid= waitpid(-1, &stat ,WNOHANG))> 0 ) //0: WAIT_MYGRP, -1: WAIT_ANY
 		printf("%d done \n", pid);
 	return;
+
+
 }
+
 
 int main()
 {
@@ -32,19 +36,19 @@ int main()
     catch_sigint(SIGINT);
     catch_sigtstp(SIGTSTP);  
 
-    signal(SIGCHLD,bg_handler);
-
+    signal(SIGCHLD,bg_handler);	
 
   while (1) {
-
+   
     printf("mysh :");//dd
+    
     fgets(buf, 8096, stdin);
 
 
     struct single_command commands[512];
     int n_commands = 0;
     mysh_parse_command(buf, &n_commands, &commands);
-	
+
     int ret = evaluate_command(n_commands, &commands);
 
     free_commands(n_commands, &commands);
